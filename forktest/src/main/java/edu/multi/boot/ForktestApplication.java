e186d10a3c7d4945450a93ae44ac52a588f98871
student <student@M507INS>
2021-09-14 12:42:59 +0900

    1 commit

---
+++ forktest/src/main/java/edu/multi/boot/ForktestApplication.java
@@ -0,0 +1,14 @@
+package edu.multi.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForktestApplication.class, args);
+		System.out.println("forktest");
+	}
+
+}
